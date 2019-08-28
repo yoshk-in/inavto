0c4b3ddd7a9890c19269619e5ee393ebaa68c120
Vadim7423 <vadalm@mail.ru>
2019-08-28 20:19:29 +0300

    on server 28.08.2019

--- backend/config/main.php
+++ backend/config/main.php
@@ -38,13 +38,6 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-                
-            ],
-        ],
     ],
     'controllerMap' => [
         'elfinder' => [
